ae79311cd18f8047bb8fdae632bb5edce0ba2a25
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-03-31 17:52:27 -0300

    feat: Provide store to App
    Pass store to Provider component that is wrapping the App itself.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+import reducer from 'reducers'
+import middleware from 'middlewares'
 import App from 'components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(reducer, middleware)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+)
