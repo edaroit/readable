10b872190c1c1820b7790796cc803cdba6ea5274
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-03-31 21:54:14 -0300

    feat: Add selector for posts by category
    Add selector to access store data.

--- src/selectors/posts.js
+++ src/selectors/posts.js
@@ -1,1 +1,14 @@
-export const getPosts = state => state.posts.posts
+import { createSelector } from 'reselect'
+
+const postsSelector = state => state.posts.posts
+
+const filterPostByCategory = (posts, category) =>
+  posts.filter(post => post.category === category)
+
+export const getPosts = postsSelector
+
+export const getPostsByCategory = category =>
+  createSelector(
+    postsSelector,
+    posts => filterPostByCategory(posts, category),
+  )
