27a74a179f46b3a7827de2886572f13919779f1a
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-03-31 21:55:54 -0300

    feat: Add selector for posts by id
    Add selector to access store data.

--- src/selectors/posts.js
+++ src/selectors/posts.js
@@ -5,10 +5,18 @@ const postsSelector = state => state.posts.posts
 const filterPostByCategory = (posts, category) =>
   posts.filter(post => post.category === category)
 
+const filterPostById = (posts, id) => posts.filter(post => post.id === id)
+
 export const getPosts = postsSelector
 
 export const getPostsByCategory = category =>
   createSelector(
     postsSelector,
     posts => filterPostByCategory(posts, category),
+  )
+
+export const getPostsById = id =>
+  createSelector(
+    postsSelector,
+    posts => filterPostById(posts, id),
   )
