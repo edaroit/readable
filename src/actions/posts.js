d58804ae9f1029513220205e9a7aac72e09ab453
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-03-31 21:26:19 -0300

    feat: Add savePost action creator
    Add save related actions to post actions.

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,9 +1,12 @@
 import { createAction } from 'redux-actions'
-import { getPosts } from 'utils/api'
+import { getPosts, postPosts } from 'utils/api'
 
 export const fetchPostsRequest = createAction('FETCH_POSTS_REQUEST')
 export const fetchPostsSuccess = createAction('FETCH_POSTS_SUCCESS')
 export const fetchPostsFailure = createAction('FETCH_POSTS_FAILURE')
+export const savePostsRequest = createAction('SAVE_POSTS_REQUEST')
+export const savePostsSuccess = createAction('SAVE_POSTS_SUCCESS')
+export const savePostsFailure = createAction('SAVE_POSTS_FAILURE')
 
 export const loadPosts = () => async dispatch => {
   dispatch(fetchPostsRequest())
@@ -14,5 +17,16 @@ export const loadPosts = () => async dispatch => {
   } catch (exception) {
     const error = exception.message
     return dispatch(fetchPostsFailure({ error }))
+  }
+}
+
+export const savePost = post => async dispatch => {
+  dispatch(savePostsRequest())
+  try {
+    await postPosts(post)
+    return dispatch(savePostsSuccess(post))
+  } catch (exception) {
+    const error = exception.message
+    return dispatch(savePostsFailure({ error }))
   }
 }
