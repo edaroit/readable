319dc4bfcba9bbd35764cb9c725f7aefc6974874
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-04-02 18:39:13 -0300

    feat: Add posts to Dashboard component

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -2,20 +2,32 @@ import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
+import Card from 'components/Card'
 import Chip from 'components/Chip'
 
 import { loadCategories } from 'actions/categories'
+import { loadPosts } from 'actions/posts'
 import { getCategories } from 'selectors/categories'
+import { getPosts } from 'selectors/posts'
 
-const Dashboard = ({ loadCategories, categories = [] }) => {
+const Dashboard = ({
+  loadCategories,
+  loadPosts,
+  categories = [],
+  posts = [],
+}) => {
   useEffect(() => {
     loadCategories()
+    loadPosts()
   }, [])
 
   return (
     <div>
       {categories.map(category => (
         <Chip key={category.name}>{category.name}</Chip>
+      ))}
+      {posts.map(post => (
+        <Card key={post.id}>{post.title}</Card>
       ))}
     </div>
   )
@@ -24,11 +36,13 @@ const Dashboard = ({ loadCategories, categories = [] }) => {
 export default connect(
   state => ({
     categories: getCategories(state),
+    posts: getPosts(state),
   }),
   dispatch =>
     bindActionCreators(
       {
         loadCategories,
+        loadPosts,
       },
       dispatch,
     ),
