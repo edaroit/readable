ba089fea18934e847c46bc62569a37472b5d4e2a
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-04-02 19:05:04 -0300

    refactor: Enhance Chip styles

--- src/components/Chip/index.js
+++ src/components/Chip/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 import './chip.scss'
 
-const Chip = ({ children }) => <div>{children}</div>
+const Chip = ({ children }) => <div className="chip">{children}</div>
 
 export default Chip
