08d44d7c755b3fba3671b30e5c59518b6c7a0368
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-04-02 19:31:42 -0300

    refactor: Update style for Dashboard

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -11,6 +11,10 @@ import { loadPosts } from 'actions/posts'
 import { getCategories } from 'selectors/categories'
 import { getPosts } from 'selectors/posts'
 
+import { formatDate } from 'utils/formatter'
+
+import './dashboard.scss'
+
 const Dashboard = ({
   loadCategories,
   loadPosts,
@@ -23,15 +27,33 @@ const Dashboard = ({
   }, [])
 
   return (
-    <div>
-      {categories.map(category => (
-        <Chip key={category.name}>{category.name}</Chip>
-      ))}
-      <Button>New Post</Button>
-      {posts.map(post => (
-        <Card key={post.id}>{post.title}</Card>
-      ))}
-    </div>
+    <main className="dashboard">
+      <h1 className="dashboard__title">Readable</h1>
+      <section className="flex justify-between dashboard__categories">
+        {categories.map(category => (
+          <Chip key={category.name}>{category.name}</Chip>
+        ))}
+      </section>
+      <section className="flex justify-end dashboard__buttons">
+        <Button>New Post</Button>
+      </section>
+      <section className="dashboard__posts">
+        {posts.map(post => (
+          <Card className="post" direction="column" key={post.id}>
+            <h3 className="post__title">{post.title}</h3>
+            <span className="post__sub-title">{post.author}</span>
+            <span className="post__sub-title">
+              {formatDate(post.timestamp)}
+            </span>
+            <span className="post__sub-title">{post.category}</span>
+            <div className="post__body">
+              <span>{post.body}</span>
+            </div>
+            <span>{post.voteScore} Vote Score</span>
+          </Card>
+        ))}
+      </section>
+    </main>
   )
 }
 
