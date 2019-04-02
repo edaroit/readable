e8e041b44475c796008aec2b60a7240b3b2566a6
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-04-02 19:20:29 -0300

    feat: Add dates module with createDate

---
+++ src/utils/dates.js
@@ -0,0 +1,8 @@
+import moment from 'moment'
+
+export const createDate = (stringDate, options = {}) => {
+  if (stringDate === null) return null
+  const { timezone = '' } = options
+  if (timezone === 'utc') return moment.utc(stringDate)
+  return moment(stringDate)
+}
