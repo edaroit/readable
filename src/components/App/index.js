3e304682a534500a3013e7a0260362ecd4bab2bd
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-04-03 19:28:34 -0300

    feat: Add Router to App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Dashboard from 'components/Dashboard'
 
-const App = () => <Dashboard />
+const App = () => (
+  <Router>
+    <Route path="/">
+      <Dashboard />
+    </Route>
+  </Router>
+)
 
 export default App
