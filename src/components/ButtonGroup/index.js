6102199bce5fafd82ec49ed078193967f7a61dff
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-04-06 22:00:17 -0300

    feat: Add ButtonGroup component

---
+++ src/components/ButtonGroup/index.js
@@ -0,0 +1,27 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React from 'react'
+import cn from 'classnames'
+
+import './button-group.scss'
+
+export const ButtonGroupItem = ({ selected, onClick, children }) => (
+  <div
+    className={cn(
+      'flex',
+      'items-center',
+      'justify-center',
+      'button-group__item',
+      selected && 'button-group__item--selected',
+    )}
+    onClick={onClick}
+  >
+    {children}
+  </div>
+)
+
+const ButtonGroup = ({ className, children }) => (
+  <div className={cn('button-group', className && className)}>{children}</div>
+)
+
+export default ButtonGroup
