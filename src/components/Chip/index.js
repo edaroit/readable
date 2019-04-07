91f6c201fff26f967daf26a09a93343927b60122
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-04-06 23:18:14 -0300

    feat: Add selected and onClick props to Chip

--- src/components/Chip/index.js
+++ src/components/Chip/index.js
@@ -1,7 +1,14 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react'
+import cn from 'classnames'
 
 import './chip.scss'
 
-const Chip = ({ children }) => <div className="chip">{children}</div>
+const Chip = ({ children, selected, onClick }) => (
+  <div className={cn('chip', selected && 'chip--selected')} onClick={onClick}>
+    {children}
+  </div>
+)
 
 export default Chip
