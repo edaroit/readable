583956d1ddee88a28f6655e151b4cce10d816481
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-04-07 13:47:58 -0300

    feat: Add inputs and submit for NewPost

--- src/components/NewPost/index.js
+++ src/components/NewPost/index.js
@@ -1,5 +1,80 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import uuid from 'uuid/v4'
 
-const NewPost = () => <div />
+import Button from 'components/Button'
+import Title from 'components/Title'
 
-export default NewPost
+import { savePost } from 'actions/posts'
+import { loadCategories } from 'actions/categories'
+import { getCategories } from 'selectors/categories'
+
+import { timestampNow } from 'utils/dates'
+
+const NewPost = ({ savePost, loadCategories, categories }) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [category, setCategory] = useState('')
+  const [body, setBody] = useState('')
+  const [isSubmitted, setIsSubmitted] = useState(false)
+
+  useEffect(() => {
+    loadCategories()
+  }, [])
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    savePost({
+      id: uuid(),
+      title,
+      author,
+      category,
+      body,
+      timestamp: timestampNow(),
+    })
+    setIsSubmitted(true)
+  }
+
+  return isSubmitted ? (
+    <Redirect to="/" />
+  ) : (
+    <form className="flex flex-column" onSubmit={handleSubmit}>
+      <header className="flex justify-between">
+        <Title>readable</Title>
+        <Button type="submit">publish</Button>
+      </header>
+      <article className="flex">
+        <input
+          name="title"
+          value={title}
+          onChange={({ target }) => setTitle(target.value)}
+        />
+        <input
+          name="author"
+          value={author}
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+        <select
+          value={category}
+          onChange={({ target }) => setCategory(target.value)}
+        >
+          {categories.map(c => (
+            <option value={c.name}>{c.name}</option>
+          ))}
+        </select>
+        <input
+          name="body"
+          value={body}
+          onChange={({ target }) => setBody(target.value)}
+        />
+      </article>
+    </form>
+  )
+}
+
+export default connect(
+  state => ({ categories: getCategories(state) }),
+  dispatch => bindActionCreators({ savePost, loadCategories }, dispatch),
+)(NewPost)
