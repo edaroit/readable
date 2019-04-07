0c1eeb471a6939480cebc444984fae5fc72d71a7
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-04-07 16:18:54 -0300

    refactor: Remove reselect

--- src/selectors/comments.js
+++ src/selectors/comments.js
@@ -1,14 +1,4 @@
-import { createSelector } from 'reselect'
-
 const commentsSelector = state =>
   state.comments.comments.filter(comment => comment.deleted === false)
 
-const filterPostById = (comments, id) => comments.filter(post => post.id === id)
-
-export const getPosts = commentsSelector
-
-export const getPostsById = id =>
-  createSelector(
-    commentsSelector,
-    comments => filterPostById(comments, id),
-  )
+export const getComments = commentsSelector
--- src/selectors/posts.js
+++ src/selectors/posts.js
@@ -1,14 +1,7 @@
-import { createSelector } from 'reselect'
-
 const postsSelector = state =>
   state.posts.posts.filter(post => post.deleted === false)
 
-const filterPostById = (posts, id) => posts.filter(post => post.id === id)
+const findPostById = (posts, id) => posts.find(post => post.id === id)
 
 export const getPosts = postsSelector
-
-export const getPostsById = id =>
-  createSelector(
-    postsSelector,
-    posts => filterPostById(posts, id),
-  )
+export const getPostById = (state, id) => findPostById(postsSelector(state), id)
