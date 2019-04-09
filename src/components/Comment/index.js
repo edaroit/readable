63006b0b6cfc3d09810aabf16a557d73951eab7c
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-04-09 18:27:53 -0300

    feat: Add Actions to Comment

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -1,19 +1,32 @@
 import React from 'react'
 
+import Actions from 'components/Actions'
 import Vote from 'components/Vote'
 
 import { formatDate } from 'utils/formatter'
 
 import './comment.scss'
 
-const Comment = ({ id, author, timestamp, body, voteScore, onVote }) => (
+const Comment = ({
+  id,
+  author,
+  timestamp,
+  body,
+  voteScore,
+  onVote,
+  onEdit,
+  onDelete,
+}) => (
   <article className="flex flex-column comment">
     <div className="flex items-center comment__information">
       <span>{author}</span>
       <span>{formatDate(timestamp)}</span>
     </div>
     <div className="comment__body">{body}</div>
-    <Vote id={id} voteScore={voteScore} onVote={onVote} direction="row" />
+    <div className="flex justify-between">
+      <Actions id={id} onEdit={onEdit} onDelete={onDelete} direction="row" />
+      <Vote id={id} voteScore={voteScore} onVote={onVote} direction="row" />
+    </div>
   </article>
 )
 
