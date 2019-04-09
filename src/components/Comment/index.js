1841de88f4b8b47afa5d24e35001a17098bb5cda
Eliseu Daroit <eliseu.daroit@gmail.com>
2019-04-09 19:59:13 -0300

    feat: Add edit to Comment component

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Actions from 'components/Actions'
+import EditComment from 'components/EditComment'
 import Vote from 'components/Vote'
 
 import { formatDate } from 'utils/formatter'
@@ -16,18 +17,37 @@ const Comment = ({
   onVote = () => {},
   onEdit = () => {},
   onDelete = () => {},
-}) => (
-  <article className="flex flex-column comment">
-    <div className="flex items-center comment__information">
-      <span>{author}</span>
-      <span>{formatDate(timestamp)}</span>
-    </div>
-    <div className="comment__body">{body}</div>
-    <div className="flex justify-between">
-      <Actions id={id} onEdit={onEdit} onDelete={onDelete} direction="row" />
-      <Vote id={id} voteScore={voteScore} onVote={onVote} direction="row" />
-    </div>
-  </article>
-)
+}) => {
+  const [isEditting, setIsEditting] = useState(false)
+
+  const handleEdit = () => {
+    setIsEditting(true)
+  }
+
+  const handleSubmit = () => {
+    setIsEditting(false)
+  }
+
+  return isEditting ? (
+    <EditComment onSubmit={handleSubmit} comment={{ id, author, body }} />
+  ) : (
+    <article className="flex flex-column comment">
+      <div className="flex items-center comment__information">
+        <span>{author}</span>
+        <span>{formatDate(timestamp)}</span>
+      </div>
+      <div className="comment__body">{body}</div>
+      <div className="flex justify-between">
+        <Actions
+          id={id}
+          onEdit={handleEdit}
+          onDelete={onDelete}
+          direction="row"
+        />
+        <Vote id={id} voteScore={voteScore} onVote={onVote} direction="row" />
+      </div>
+    </article>
+  )
+}
 
 export default Comment
